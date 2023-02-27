c354de6dd799290cc4afcd3c52af61fe75bcf975
Arnav Malhotra <58338755+Arnav-MaIhotra@users.noreply.github.com>
2023-02-26 19:24:16 -0500

    Delete Scholar_GUI.py

--- Scholar_GUI.py
+++
@@ -1,8 +0,0 @@
-import requests, bs4, tkinter as tk; from tkinter.scrolledtext import ScrolledText
-def search(key):
-    [answer.insert(tk.END, p.text) for url in [f"https://en.wikipedia.org/wiki/{key.replace(' ', '_')}", f"https://www.britannica.com/search?query={key.replace(' ', '%20')}"] for p in bs4.BeautifulSoup(requests.get(url).content, 'html.parser').find_all(['p', 'p', {'class': 'topic-paragraph'}])]
-    answer.configure(state='disabled')
-window, searchterm, button, answer = tk.Tk(), tk.Entry(), tk.Button(text="Search"), ScrolledText(width=80, height=30)
-button.bind("<Button-1>", lambda event: search(searchterm.get()))
-searchterm.pack(); button.pack(); answer.pack()
-window.mainloop()
